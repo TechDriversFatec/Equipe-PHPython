af642909d7571838f589c99c656520c45e6b0103
Giovanni Guidace <giovannimarassi@gmail.com>
2020-05-19 10:09:20 -0300

    Novo front end

---
+++ pi/gantt/templates/novo_front/js/javascript.js
@@ -0,0 +1,13 @@
+
+function expandeTarefas() {
+    col_tarefas.innerHTML = col_tarefas.innerHTML + "<button type='button' class='btn_shadow3'>TAREFAS 1</button>";
+    
+}
+
+function expandePessoas() {
+    pessoas_list.innerHTML = pessoas_list.innerHTML + "<label class='styleWord1'>DEV 1</label>"
+}
+
+
+/*VISUALIZAÇÃO DO GRÁFICO GANTT*/
+
