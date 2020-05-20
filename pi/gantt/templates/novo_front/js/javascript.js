e7696aec6f0e8ae5d0d7220d6b74d530e69b9e50
Giovanni Guidace <giovannimarassi@gmail.com>
2020-05-19 22:41:03 -0300

    acrecentando grafico gantt e ajustes no Front

--- pi/gantt/templates/novo_front/js/javascript.js
+++ pi/gantt/templates/novo_front/js/javascript.js
@@ -11,3 +11,103 @@ function expandePessoas() {
 
 /*VISUALIZAÇÃO DO GRÁFICO GANTT*/
 
+function carregaGantt(){
+    tasks = [
+  {
+    id: 'Task 1',
+    name: 'Redesign website',
+    start: '2020-05-20',
+    end: '2020-05-25',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone' // optional
+  },
+        {
+    id: 'Task 1',
+    name: 'Redesign website',
+    start: '2020-05-20',
+    end: '2020-05-25',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone' // optional
+  },
+        {
+    id: 'Task 1',
+    name: 'Redesign website',
+    start: '2020-05-20',
+    end: '2020-05-25',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone' // optional
+  },
+        {
+    id: 'Task 1',
+    name: 'Redesign website',
+    start: '2020-05-20',
+    end: '2020-05-25',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone' // optional
+  },
+        {
+    id: 'Task 1',
+    name: 'Redesign website',
+    start: '2020-05-20',
+    end: '2020-05-25',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone' // optional
+  },
+        {
+    id: 'Task 1',
+    name: 'Redesign website',
+    start: '2020-05-20',
+    end: '2020-05-25',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone' // optional
+  },
+         {
+    id: 'Task 1',
+    name: 'Redesign website',
+    start: '2020-05-20',
+    end: '2020-05-25',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone' // optional
+  },
+         {
+    id: 'Task 1',
+    name: 'Redesign website',
+    start: '2020-05-20',
+    end: '2020-05-25',
+    progress: 20,
+    dependencies: 'Task 2, Task 3',
+    custom_class: 'bar-milestone' // optional
+  }
+    
+]
+    
+    
+    
+  gantt = new Gantt("#gantt", tasks);
+    
+    var gantt = new Gantt("#gantt", tasks, {
+    header_height: 50,
+    column_width: 30,
+    step: 24,
+    view_modes: ['Quarter Day', 'Half Day', 'Day', 'Week', 'Month'],
+    bar_height: 20,
+    bar_corner_radius: 3,
+    arrow_curve: 5,
+    padding: 18,
+    view_mode: 'Day',   
+    date_format: 'YYYY-MM-DD',
+    custom_popup_html: null
+});
+    
+}
+
+
+
+window.onload = carregaGantt;
