6556ad38db7a1c5c407d877a3b422c900397c362
Giovanni Guidace <giovannimarassi@gmail.com>
2020-05-20 17:55:57 -0300

    criação de menu drop down

--- pi/gantt/templates/novo_front/js/javascript.js
+++ pi/gantt/templates/novo_front/js/javascript.js
@@ -8,6 +8,30 @@ function expandePessoas() {
     pessoas_list.innerHTML = pessoas_list.innerHTML + "<label class='styleWord1'>DEV 1</label>"
 }
 
+/*MENU DROPDOWN BTN1*///////////////////////////////
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function menuDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+     dropdowns = document.getElementsByClassName("dropdown-content");
+     
+    for (i = 0; i < dropdowns.length; i++) {
+         openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+//////////////////////////////////////////////////
+
 
 /*VISUALIZAÇÃO DO GRÁFICO GANTT*/
 
