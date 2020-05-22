9c626a1db49d673b93d35d606c0c0d9bd39bf760
Giovanni Guidace <giovannimarassi@gmail.com>
2020-05-22 17:47:35 -0300

    criação do cadastro de pessoas(nao finalizado)

--- pi/gantt/templates/novo_front/js/javascript.js
+++ pi/gantt/templates/novo_front/js/javascript.js
@@ -1,9 +1,70 @@
 /*POPUP WINDOW*/
 
-function cadastroProjeto(){
-popupCadastroProjeto = window.open('', 'pagina', "width=250 height=250 left=700 top=280");
-popupCadastroProjeto.document.write ("Aqui você pode por o que quiser");
+
+/*CADASTRO DE PESSOAS*/
+/*function cadastroPessoas(){
+popupCadastroProjeto = window.open('', 'Cadastro de Pessoas', "width=250 height=250 left=700 top=280 resizable=false scrollbars=false");
+popupCadastroProjeto.document.write ("<!DOCTYPE HTML><html><head><meta charset='utf-8'><title>Cadastro de pessoas</title><link rel='stylesheet' href='css/estilos.css'></head><body><div id='cadastroPessoas'><label for='nomePessoa'>Nome da pessoa: </label><input type='text' id='nomePessoa'><br><label for='contato'>Contato: </label><input type='text' id='contato'><br><button id='btn_gravarpessoa' class='btn_shadow2'>Gravar</button><button id='btn_limpar' class='btn_shadow2'>Limpar</button></div><div id='listaPessoas'><table border=1><thead><caption>Pessoas cadastradas:</caption><tr><th>Nome</th><th>Contato</th></tr></thead><tbody><tr><td>Pedrinho</td><td>(12)99999-9999</td></tr><tr><td>João</td><td>(12)99999-9999</td></tr></tbody></table></div></body> </html>");
+}*/
+
+/*
+clicaCadastroPessoas = document.getElementById("clicaPessoas");
+abreCadastroPessoas = document.getElementById("abreCadastroPessoas")
+
+
+clicaCadastroPessoas.addEventListener.apply('click', function(){
+    abreCadastroPessoas[0].show();                     
+                         
+});*/
+
+
+function clicaPessoas(){
+    
+    dialogCadastro = document.getElementById("abreCadastroPessoas");
+    
+    document.getElementById("menu_superior").classList.remove("show");
+    dialogCadastro.showModal();
 }
+
+
+function gravarPessoa(){
+    
+    nomePessoa = document.getElementById("nomePessoa");
+    contato = document.getElementById("contato");
+    
+    
+    
+    divEl = document.createElement("tbody")
+    textEl = document.createTextNode("<tr><td>"+nomePessoa.value+"</td><td>"+contato.value+"</td></tr>")
+    divEl.appendChild(textEl)
+
+    document.getElementById("listaPessoas").appendChild(divEl)
+    
+    
+    /*
+    novoConteudo = document.createTextNode("<tr><td>"+nomePessoa.value+"</td><td>"+contato.value+"</td></tr>");
+    document.getElementById("adicionarTabela").appendChild(novoConteudo);
+    
+    */
+    
+    
+    
+    /*selecionatabela = document.getElementById('adicionatabela');
+    selecionatabela.innerHTML = "<tr><td>"+nomePessoa.value+"</td><td>"+contato.value+"</td></tr>"*/
+    
+    /*selecionaTabela = document.querySelector('adicionaTabela');
+    
+    conteudoNovo = documen.createTextNode("<tr><td>"+nomePessoa.value+"</td><td>"+contato.value+"</td></tr>");
+    
+    fragment = new DocumentFragment();
+    
+    fragment.appendChild(conteudoNovo);*/
+    
+}
+
+
+
+/*////////////////////////////////////////*/
 
 
 ////////////////
@@ -16,26 +77,28 @@ function expandePessoas() {
     pessoas_list.innerHTML = pessoas_list.innerHTML + "<label class='styleWord1'>DEV 1</label>"
 }
 
-/*MENU DROPDOWN BTN1*///////////////////////////////
+/*MENU DROPDOWN BTN_menusuperior*/////////////////////////////
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function menuDropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function menuDropdown_menusuperior() {
+  document.getElementById("menu_superior").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-     dropdowns = document.getElementsByClassName("dropdown-content");
-     
-    for (i = 0; i < dropdowns.length; i++) {
-         openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+/*MENU PROJETO DROPDOWN*/
+
+function menuDropdown_prj() {
+  document.getElementById("menu_prj").classList.toggle("show");
+}
+
+/*MENU TAREFA DROPDOWN*/
+
+function menuDropdown_trf() {
+  document.getElementById("menu_trf").classList.toggle("show");
+}
+
+/*MENU PESSOAS DROPDOWN*/
+
+function menuDropdown_pessoas() {
+  document.getElementById("menu_pessoas").classList.toggle("show");
 }
 
 //////////////////////////////////////////////////
