8a0493f42e320f08a10f12aa7ddd96d810ae362b
Jessica Isri Dias Cruz <jessicaisri@gmail.com>
2020-06-05 21:57:23 -0300

    Criação de apis rest para os models.py

---
+++ pi/gantt/serializers.py
@@ -0,0 +1,52 @@
+from .models import tb_Projeto, tb_Tarefa, tb_Pessoa, tb_Dev_Trf
+from rest_framework import serializers
+
+
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    # url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = tb_Projeto
+        fields = (
+            'prj_id',
+            'prj_nome',
+            'prj_datainicio',
+            'prj_prazoentrega',
+            'prj_escopo',
+            'prj_color'
+        )
+
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = tb_Tarefa
+        fields = (
+            'trf_id',
+            'trf_name',
+            'trf_datainicial',
+            'trf_datafinal',
+            'trf_prazo',
+            'trf_interdependencia',
+            'trf_entregavel'
+        )
+
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = tb_Pessoa
+        fields = (
+            'pes_id',
+            'pes_nome',
+            'pes_contato'
+        )
+
+
+class DistributeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = tb_Dev_Trf
+        fields = (
+            'fk_pes_id',
+            'fk_trf_id',
+            'fk_prj_id'
+        )
